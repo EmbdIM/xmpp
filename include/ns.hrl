8e679b60e471bd5117c6d77def4069622844b786
Igor <qomputer@gmail.com>
2024-12-28 16:25:04 +0200

    Add macros NS_MESSAGE_MODERATE_0 and NS_MESSAGE_MODERATE_1

--- include/ns.hrl
+++ include/ns.hrl
@@ -212,6 +212,8 @@
 -define(NS_X509_0, <<"urn:xmpp:x509:0">>).
 -define(NS_HATS, <<"xmpp:prosody.im/protocol/hats:1">>).
 -define(NS_MESSAGE_MODERATE, <<"urn:xmpp:message-moderate:0">>).
+-define(NS_MESSAGE_MODERATE_0, <<"urn:xmpp:message-moderate:0">>).
+-define(NS_MESSAGE_MODERATE_1, <<"urn:xmpp:message-moderate:1">>).
 -define(NS_MESSAGE_RETRACT, <<"urn:xmpp:message-retract:1">>).
 -define(NS_OCCUPANT_ID, <<"urn:xmpp:occupant-id:0">>).
 -define(NS_REPLY, <<"urn:xmpp:reply:0">>).
